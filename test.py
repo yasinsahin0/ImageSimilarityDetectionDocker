2c65fa9d45f23a6f7b5e17c2987890004e6a249e
yasinsahin0 <181307026@kocaeli.edu.tr>
2022-05-20 22:15:47 +0300

    main

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ with open("tanımsız.jpg", "rb") as image_file:
     encoded_string = base64.b64encode(image_file.read())
 
 st_time = datetime.datetime.now()
-"""
+
 import requests
 
 url = "http://127.0.0.1:5000/img"
@@ -19,13 +19,9 @@ files=[
 headers = {}
 
 response = requests.request("POST", url, headers=headers, data=payload, files=files)
-"""
+
 sme = sim.Similarity()
 result = sme.black_white_result(encoded_string)
 stop_time = datetime.datetime.now()
 print("Süre : ", str(stop_time-st_time))
-
-
-
-
-#print(response.text)
+print(response.text)
